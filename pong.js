be16d8097ae1d86f9e10855e62ec4a3268c6a0fa
woz07 <muhsameer9@gmail.com>
2025-02-15 16:07:16 +0000

    chore: do x and y for all entities

--- pong.js
+++ pong.js
@@ -1,16 +1,41 @@
 const engine = require('raylib')
+
+// Screen data
 const screen = {
     width: 800,
     height: 450
 }
+
+// Players
+// TODO: Incorporate x and y coords into player1, player2 and ball
+// Player1 needs to be at the bottom
+// Player2 needs to be at the top
+// Ball must start in the middle
+const player1 = {
+    width: 10,
+    height: 50,
+    color: engine.BLUE,
+}
+
+const player2 = {
+    width: 10,
+    height: 50,
+    color: engine.RED,
+}
+
+const ball = {
+    width: 5,
+    height: 5,
+    color: engine.WHITE,
+}
+
 engine.InitWindow(screen.width, screen.height, "pong")
 engine.SetTargetFPS(60)
 
 
 while (!engine.WindowShouldClose()) {
     engine.BeginDrawing();
-    engine.ClearBackground(engine.RAYWHITE)
-    engine.DrawText("Congrats! You created your first node-raylib window!", 120, 200, 20, engine.LIGHTGRAY)
+    engine.ClearBackground(engine.BLACK)
     engine.EndDrawing()
 }
 engine.CloseWindow()
