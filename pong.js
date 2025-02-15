4487e834584c63d55c55ff784d97b40103320f08
Respinity1 <iqbalshoaib150@gmail.com>
2025-02-15 16:20:29 +0000

    add: both players

--- pong.js
+++ pong.js
@@ -15,12 +15,16 @@ const player1 = {
     width: 10,
     height: 50,
     color: engine.BLUE,
+    x: 0, 
+    y: screen.height / 2 - 25
 }
 
 const player2 = {
     width: 10,
     height: 50,
     color: engine.RED,
+    x: screen.width - 10,
+    y: screen.height / 2 - 25
 }
 
 const ball = {
@@ -29,13 +33,15 @@ const ball = {
     color: engine.WHITE,
 }
 
+
 engine.InitWindow(screen.width, screen.height, "pong")
 engine.SetTargetFPS(60)
-
 
 while (!engine.WindowShouldClose()) {
     engine.BeginDrawing();
     engine.ClearBackground(engine.BLACK)
+    engine.DrawRectangle(player1.x, player1.y, player1.width, player1.height, player1.color)
+    engine.DrawRectangle(player2.x, player2.y,  player2.width, player2.height, player2.color)
     engine.EndDrawing()
 }
 engine.CloseWindow()
